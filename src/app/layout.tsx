b4be02ed928ec4ecc885532e2ff0df73fda00e37
Samwel Otieno <otienosamwelmitto@gmail.com>
2024-01-26 18:24:34 +0000

    remove unecessary space

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,25 @@
 import './globals.css'
 import Header from "@/app/components/Header";
-import Footer, {SpaceLarge} from "@/app/components/Footer"
+import Footer, { SpaceLarge } from "@/app/components/Footer"
 
 export const metadata = {
     title: 'Samwel Otieno',
     description: 'Personal website',
 }
 
-export default function RootLayout({children,}: {
+export default function RootLayout({ children, }: {
     children: React.ReactNode
 }) {
     return (
         <html lang="en" className={"antialiased bg-black text-gray-200"}>
-        <body className={"flex flex-col justify-between"}>
-        <Header/>
-        {children}
-
-        <div>
-            <SpaceLarge/>
-        </div>
-
-        <hr className={"opacity-20 mx-auto w-1/4"}/>
-
-        <SpaceLarge/>
-        <Footer/>
-        </body>
+            <body className={"flex flex-col justify-between"}>
+                <Header />
+                    {children}
+                <SpaceLarge />
+                <hr className={"opacity-20 mx-auto w-1/4"} />
+                <SpaceLarge />
+                <Footer />
+            </body>
         </html>
     )
 }
